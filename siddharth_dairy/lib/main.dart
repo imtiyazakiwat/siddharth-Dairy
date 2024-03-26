48b0dffefae086bd0e1ed064720e5cb00cbcd493
Imtiyaz Akiwat <imtiyazakiwat0@gmail.com>
2024-03-26 22:49:45 +0530

    replaced form dialog input with excell like input style

--- siddharth_dairy/lib/main.dart
+++ siddharth_dairy/lib/main.dart
@@ -25,9 +25,16 @@ class CalculationPage extends StatefulWidget {
 
 class _CalculationPageState extends State<CalculationPage> {
   List<Map<String, dynamic>> dataList = [];
-  final TextEditingController farmerController = TextEditingController();
-  final TextEditingController litresController = TextEditingController();
-  final TextEditingController rateController = TextEditingController();
+  final List<TextEditingController> litreControllers = [];
+  final List<TextEditingController> rateControllers = [];
+  final List<FocusNode> litreFocusNodes = [];
+  final List<FocusNode> rateFocusNodes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _addRow(); // Add an initial row
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,209 +44,162 @@ class _CalculationPageState extends State<CalculationPage> {
       ),
       body: Column(
         children: [
-          _buildTable(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.length + 1, // +1 for the total row
+              itemBuilder: (context, index) {
+                if (index == dataList.length) {
+                  return _buildTotalRow();
+                } else {
+                  return _buildRow(index);
+                }
+              },
+            ),
+          ),
           _buildAddRowButton(),
-          _buildTotalsRow(),
         ],
       ),
     );
   }
 
-  Widget _buildTable() {
-    return Expanded(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: DataTable(
-          columns: [
-            DataColumn(label: Text('Farmer Name')),
-            DataColumn(label: Text('Litres')),
-            DataColumn(label: Text('Milk Rate')),
-            DataColumn(label: Text('Total Amount')),
-          ],
-          rows: dataList.asMap().entries.map((entry) {
-            final int index = entry.key;
-            final Map<String, dynamic> data = entry.value;
-            return DataRow(cells: [
-              DataCell(
-                Text(data['farmer']),
-                onTap: () => _editRow(index),
-              ),
-              DataCell(
-                Text(data['litres'].toString()),
-                onTap: () => _editRow(index),
-              ),
-              DataCell(
-                Text(data['rate'].toString()),
-                onTap: () => _editRow(index),
-              ),
-              DataCell(
-                Text((data['litres'] * data['rate']).toString()),
-                onTap: () => _editRow(index),
-              ),
-            ]);
-          }).toList(),
+  Widget _buildRow(int index) {
+    final Map<String, dynamic> data = dataList[index];
+    final litreController = litreControllers[index];
+    final rateController = rateControllers[index];
+
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: litreController,
+              focusNode: litreFocusNodes[index],
+              decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Litres'),
+              keyboardType: TextInputType.number,
+              onChanged: (_) => _calculateTotal(index),
+              onEditingComplete: () {
+                _moveToNextField(index);
+              },
+            ),
+          ),
         ),
-      ),
+        SizedBox(width: 8),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: rateController,
+              focusNode: rateFocusNodes[index],
+              decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Milk Rate'),
+              keyboardType: TextInputType.number,
+              onChanged: (_) => _calculateTotal(index),
+              onEditingComplete: () {
+                _moveToNextField(index);
+              },
+            ),
+          ),
+        ),
+        SizedBox(width: 8),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              (data['litres'] * data['rate']).toString(),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTotalRow() {
+    double totalLitres = dataList.fold(0, (sum, data) => sum + data['litres']);
+    double totalAmount = dataList.fold(0, (sum, data) => sum + (data['litres'] * data['rate']));
+
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Total',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        SizedBox(width: 8),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              totalLitres.toStringAsFixed(2),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        SizedBox(width: 8),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              totalAmount.toStringAsFixed(2),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
   Widget _buildAddRowButton() {
     return ElevatedButton(
-      onPressed: _displayAddDialog,
+      onPressed: _addRow,
       child: Icon(Icons.add),
     );
   }
 
-  Widget _buildTotalsRow() {
-    double totalLitres = dataList.fold(0, (sum, data) => sum + data['litres']);
-    double totalAmount = dataList.fold(0, (sum, data) => sum + (data['litres'] * data['rate']));
-
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Total: '),
-          Text(totalLitres.toString()),
-          SizedBox(width: 50),
-          Text(totalAmount.toString()),
-        ],
-      ),
-    );
-  }
-
-  void _displayAddDialog() {
-    farmerController.clear();
-    litresController.clear();
-    rateController.clear();
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Add Row'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: farmerController,
-                decoration: InputDecoration(labelText: 'Farmer Name'),
-              ),
-              TextField(
-                controller: litresController,
-                decoration: InputDecoration(labelText: 'Litres'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: rateController,
-                decoration: InputDecoration(labelText: 'Milk Rate'),
-                keyboardType: TextInputType.number,
-              ),
-            ],
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                _addRow();
-                Navigator.of(context).pop();
-              },
-              child: Text('Add'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _editRow(int index) {
-    final Map<String, dynamic> data = dataList[index];
-    farmerController.text = data['farmer'];
-    litresController.text = data['litres'].toString();
-    rateController.text = data['rate'].toString();
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Edit Row'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: farmerController,
-                decoration: InputDecoration(labelText: 'Farmer Name'),
-              ),
-              TextField(
-                controller: litresController,
-                decoration: InputDecoration(labelText: 'Litres'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: rateController,
-                decoration: InputDecoration(labelText: 'Milk Rate'),
-                keyboardType: TextInputType.number,
-              ),
-            ],
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                _updateRow(index);
-                Navigator.of(context).pop();
-              },
-              child: Text('Update'),
-            ),
-          ],
-        );
-      },
-    );
+  void _calculateTotal(int index) {
+    final litre = double.tryParse(litreControllers[index].text) ?? 0;
+    final rate = double.tryParse(rateControllers[index].text) ?? 0;
+    setState(() {
+      dataList[index]['litres'] = litre;
+      dataList[index]['rate'] = rate;
+    });
   }
 
   void _addRow() {
     setState(() {
-      String farmer = farmerController.text;
-      double litres = double.tryParse(litresController.text) ?? 0;
-      double rate = double.tryParse(rateController.text) ?? 0;
+      final litreController = TextEditingController();
+      final rateController = TextEditingController();
+      final litreFocusNode = FocusNode();
+      final rateFocusNode = FocusNode();
 
-      if (farmer.isNotEmpty && litres > 0 && rate > 0) {
-        dataList.add({
-          'farmer': farmer,
-          'litres': litres,
-          'rate': rate,
-        });
-      }
+      litreControllers.add(litreController);
+      rateControllers.add(rateController);
+      litreFocusNodes.add(litreFocusNode);
+      rateFocusNodes.add(rateFocusNode);
 
-      farmerController.clear();
-      litresController.clear();
-      rateController.clear();
+      dataList.add({
+        'litres': 0.0,
+        'rate': 0.0,
+      });
+
+      Future.delayed(Duration.zero, () {
+        FocusScope.of(context).requestFocus(litreFocusNodes.last);
+      });
     });
   }
 
-  void _updateRow(int index) {
-    setState(() {
-      String farmer = farmerController.text;
-      double litres = double.tryParse(litresController.text) ?? 0;
-      double rate = double.tryParse(rateController.text) ?? 0;
-
-      if (farmer.isNotEmpty && litres > 0 && rate > 0) {
-        dataList[index] = {
-          'farmer': farmer,
-          'litres': litres,
-          'rate': rate,
-        };
-      }
-
-      farmerController.clear();
-      litresController.clear();
-      rateController.clear();
-    });
-  }
-
-  @override
-  void dispose() {
-    farmerController.dispose();
-    litresController.dispose();
-    rateController.dispose();
-    super.dispose();
+  void _moveToNextField(int index) {
+    if (index == dataList.length - 1) {
+      _addRow();
+    } else {
+      FocusScope.of(context).requestFocus(index.isEven ? litreFocusNodes[index + 1] : rateFocusNodes[index + 1]);
+    }
   }
 }
